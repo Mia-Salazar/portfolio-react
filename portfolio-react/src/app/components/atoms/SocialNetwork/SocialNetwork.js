ea90c7742ad303dee39856b3a99042000f5c2cea
Mia <maria.sgr@gmail.com>
2021-08-02 13:25:30 +0200

    Fix aria-label

--- portfolio-react/src/app/components/atoms/SocialNetwork/SocialNetwork.js
+++ portfolio-react/src/app/components/atoms/SocialNetwork/SocialNetwork.js
@@ -1,12 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+import i18n from "../../../../i18n";
 
 import "./SocialNetwork.scss";
 
-export const SocialNetwork = ({icon, link, aria}) => {
+export const SocialNetwork = ({icon, link, ariaLabel}) => {
 	return (
 		<li className="social-item">
-			<a className="social-item__link" href={link} target="_blank" aria-label={aria} rel="noreferrer noopener">
+			<a className="social-item__link" href={link} target="_blank"
+				aria-label={i18n.t("footer", { key: ariaLabel})} rel="noreferrer noopener"
+			>
 				<i className={`fab social-item__icon ${icon}`}></i>
 			</a>
 		</li>
@@ -18,5 +21,5 @@ export default SocialNetwork;
 SocialNetwork.propTypes = {
 	icon: PropTypes.string.isRequired,
 	link: PropTypes.string.isRequired,
-	aria: PropTypes.string.isRequired,
+	ariaLabel: PropTypes.string.isRequired,
 };
--- portfolio-react/src/app/components/molecules/Footer/Footer.js
+++ portfolio-react/src/app/components/molecules/Footer/Footer.js
@@ -5,17 +5,17 @@ import { SocialNetwork } from "../../atoms/SocialNetwork/SocialNetwork";
 
 export const Footer = () => {
 	const socialNetwork = [
-		{icon: "fa-github-alt", link: "https://github.com/Mia-Salazar", aria: ""},
-		{icon: "fa-codepen", link: "https://codepen.io/MiaSalazar", aria: ""},
-		{icon: "fa-linkedin-in", link: "https://www.linkedin.com/in/miasalazar/", aria: ""},
-		{icon: "fa-medium-m", link: "https://marasalazar.medium.com/", aria: ""}
+		{icon: "fa-github-alt", link: "https://github.com/Mia-Salazar", aria: "Github"},
+		{icon: "fa-codepen", link: "https://codepen.io/MiaSalazar", aria: "Codepen"},
+		{icon: "fa-linkedin-in", link: "https://www.linkedin.com/in/miasalazar/", aria: "Linkedin"},
+		{icon: "fa-medium-m", link: "https://marasalazar.medium.com/", aria: "Medium"}
 	];
 	return (
 		<footer className="footer">
 			<ul className="footer__social-container">
 				{
 					socialNetwork.map((social) => {
-						return <SocialNetwork key={social.icon} icon={social.icon} link={social.link} aria={social.aria}></SocialNetwork>;
+						return <SocialNetwork key={social.icon} icon={social.icon} link={social.link} ariaLabel={social.aria}></SocialNetwork>;
 					})
 				}
 			</ul>
