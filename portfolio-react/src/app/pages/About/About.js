34693aae8143ac1431389ee41eb67247e4f50916
Mia <maria.sgr@gmail.com>
2021-08-14 15:48:45 +0200

    Add about me page

--- portfolio-react/src/app/pages/About/About.js
+++ portfolio-react/src/app/pages/About/About.js
@@ -2,32 +2,16 @@ import React from "react";
 
 import { Title } from "../../components/atoms/Title/Title";
 import { Box } from "../../components/atoms/Box/Box";
+import i18n from "../../../i18n";
 import "./About.scss";
 
 export const About = () => {
 	return (
 		<Box>
-			<Title text="footer"/>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
-			<p style={{color: "white"}}>HOLAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</p>
+			<Title text="navbar.about"/>
+			<p className="text">{i18n.t("about.textOne")}</p>
+			<p className="text">{i18n.t("about.textTwo")}</p>
+			<p className="text">{i18n.t("about.textThree")}</p>
 		</Box>
 	);
 };
