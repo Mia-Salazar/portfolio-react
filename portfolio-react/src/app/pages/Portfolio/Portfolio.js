325e59c92492b8873b4ec7fbe4d6049742e6974e
Mia <maria.sgr@gmail.com>
2021-08-18 19:17:52 +0200

    Add all elements to portfolio and improve styles

--- portfolio-react/src/app/pages/Portfolio/Portfolio.js
+++ portfolio-react/src/app/pages/Portfolio/Portfolio.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Title } from "../../components/atoms/Title/Title";
 import { Box } from "../../components/atoms/Box/Box";
+import { PortfolioItem } from "../../components/molecules/PortfolioItem/PortfolioItem";
+import { portfolioArray } from "../../utils/portfolioContent";
 import i18n from "../../../i18n";
 import "./Portfolio.scss";
 
@@ -10,6 +12,15 @@ export const Portfolio = () => {
 		<Box>
 			<Title text="navbar.portfolio"/>
 			<p className="text">{i18n.t("portfolio.subtitle")}</p>
+			<div className="portfolio-container">
+				{
+					portfolioArray.map((item, index) => {
+						return(
+							<PortfolioItem item={item} key={index}/>
+						);
+					})
+				}
+			</div>
 		</Box>
 	);
 };
---
+++ portfolio-react/src/app/utils/portfolioContent.js
@@ -0,0 +1,33 @@
+import marvel from "../../assets/images/comic.PNG";
+import lotr from "../../assets/images/lotr.jpg";
+import spotiapp from "../../assets/images/spotiapp.jpg";
+import movie from "../../assets/images/movie.jpg";
+import crusha from "../../assets/images/crusha.jpg";
+import wizard from "../../assets/images/wizard.jpg";
+import todo from "../../assets/images/todo.jpg";
+import teresa from "../../assets/images/teresa.png";
+import movies from "../../assets/images/movies.png";
+import happets from "../../assets/images/happets-reviews.png";
+import happetsBis from "../../assets/images/happets-2.png";
+import game from "../../assets/images/game.png";
+import fleetLanding from "../../assets/images/fleeet-landing.png";
+import fleetCatalog from "../../assets/images/fleeet-catalog.png";
+import XCJ from "../../assets/images/xcj.png";
+
+export const portfolioArray = [
+	{ alt:"", href: "https://github.com/Mia-Salazar/marvel-react", text:"React, React Hooks, HTML5, CSS3, SASS, Flexbox, Grid, Semantic UI, Javascript, API Rest", src: marvel, angular: false, vue: false, react: true },
+	{ alt:"", href: "https://github.com/Mia-Salazar/shop-redux", text:"Angular, Typescript, Tailwind, Redux, API Rest, CSS3, HTML5, SASS, Grid, Flexbox", src: lotr, angular: true, vue: false, react: false },
+	{ alt:"", href: "https://github.com/Mia-Salazar/spotiapp", text:"Angular, Typescript, API Rest, CSS3, HTML5, SASS, Grid, Flexbox, Boostrap 4", src: spotiapp, angular: true, vue: false, react: false },
+	{ alt:"", href: "https://github.com/Mia-Salazar/media-searcher", text:"Angular, Typescript, API Rest, CSS3, HTML5, SASS, Grid, Flexbox, Boostrap 4, Materialize", src: movie, angular: true, vue: false, react: false },
+	{ alt:"", href: "https://github.com/Mia-Salazar/youtube-angular", text:"Angular, Typescript, API Rest, CSS3, HTML5, SASS, Grid, Flexbox, Boostrap 4", src: crusha, angular: true, vue: false, react: false },
+	{ alt:"", href: "https://github.com/Mia-Salazar/crud-heroes", text:"Angular, Typescript, API Rest, CSS3, HTML5, SASS, Grid, Flexbox, Boostrap 4, Materialize", src: wizard, angular: true, vue: false, react: false },
+	{ alt:"", href: "https://github.com/Mia-Salazar/redux-todo", text:"Angular, Typescript, Redux, CSS3, HTML5, SASS, Grid, Flexbox", src: todo, angular: true, vue: false, react: false },
+	{ alt:"", href: "http://teresa-salazar.surge.sh/", text:"Reactjs, Javascript, CSS3, HTML5, BEM, SASS, Grid, Flexbox, i18n, Classnames, librerías, Formspree, lazy load", src: teresa, angular: false, vue: false, react: true },
+	{ alt:"", href: "http://mia-search-movies-project.surge.sh/", text:"Reactjs, Javascript, Bulma, CSS3, HTML5, CSS3, Flexbox", src: movies, angular: false, vue: false, react: true },
+	{ alt:"", href: "https://www.happets.com/opiniones", text:"JavaScript, jQuery, SASS, HTML5, CSS3, Flexbox, Grid, Bootstrap 4", src: happets, angular: false, vue: false, react: false },
+	{ alt:"", href: "https://www.happets.com/login", text:"React, React hooks, Gatsby, storybook, testing, i18n, JavaScript, SASS, HTML5, CSS3, Flexbox, Grid", src: happetsBis, angular: false, vue: false, react: true },
+	{ alt:"", href: "http://mia-tic-tac-toe.surge.sh/", text:"React Hooks, styled components, i18n, proptypes, classnames, HTML5, CSS3, SASS", src: game, angular: false, vue: false, react: true },
+	{ alt:"", href: "https://www.myfleeet.com/", text:"Vuejs, Nuxtjs, SASS, jQuery, HTML5, CSS3, Flexbox, metodología BEM, Pug, vue-i18n", src: fleetLanding, angular: false, vue: true, react: false },
+	{ alt:"", href: "https://www.myfleeet.com/client/catalog", text:"Vuejs, Nuxtjs, SASS, jQuery,HTML5, CSS3, Flexbox,metodología BEM, Pug, vue-i18n", src: fleetCatalog, angular: false, vue: true, react: false },
+	{ alt:"", href: "http://xcjgames.com/", text:"jQuery, Bootstrap 4, HTML5, CSS3, Flexbox, diseño", src: XCJ, angular: false, vue: false, react: false },
+];
