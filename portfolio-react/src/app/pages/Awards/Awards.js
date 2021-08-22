87cd7290b625494ba3210c6071bd1bd86e581a70
Mia <maria.sgr@gmail.com>
2021-08-22 19:05:08 +0200

    Add awarditem component

--- portfolio-react/src/app/pages/Awards/Awards.js
+++ portfolio-react/src/app/pages/Awards/Awards.js
@@ -2,6 +2,8 @@ import React from "react";
 
 import { Title } from "../../components/atoms/Title/Title";
 import { Box } from "../../components/atoms/Box/Box";
+import { AwardItem } from "../../components/molecules/AwardItem/AwardItem";
+import { awardsArray } from "../../utils/awardsArray"; 
 import i18n from "../../../i18n";
 import "./Awards.scss";
 
@@ -10,6 +12,15 @@ export const Awards = () => {
 		<Box>
 			<Title text="navbar.awards"/>
 			<p className="text">{i18n.t("awards.textOne")}</p>
+			<ul>
+				{
+					awardsArray.map((item, index) => {
+						return(
+							<AwardItem item={item} key={index}/>
+						);
+					})
+				}
+			</ul>
 		</Box>
 	);
 };
