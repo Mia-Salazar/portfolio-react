a1e1fd106e53c2b06a21a2797437e2260e55259e
Mia <maria.sgr@gmail.com>
2021-08-24 20:21:10 +0200

    Fix outline

--- portfolio-react/src/app/components/atoms/Input/Input.js
+++ portfolio-react/src/app/components/atoms/Input/Input.js
@@ -1,13 +1,12 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import i18n from "../../../../i18n";
 import "./Input.scss";
 
-export const Input = ({type, pattern, placeholder, id, value, required, functionality}) => {
+export const Input = ({type, pattern, id, value, required, functionality}) => {
 	return (
 		<input className="input" type={type} pattern={pattern} id={id} name={name} value={value}
-			placeholder={i18n.t(placeholder)} required={required} onChange={functionality}
+			required={required} onChange={functionality}
 		/>
 	);
 };
@@ -16,7 +15,6 @@ Input.propTypes = {
 	type: PropTypes.string.isRequired,
 	pattern: PropTypes.string,
 	value: PropTypes.string,
-	placeholder: PropTypes.string.isRequired,
 	id: PropTypes.string.isRequired,
 	required: PropTypes.bool.isRequired,
 	functionality: PropTypes.func.isRequired,
