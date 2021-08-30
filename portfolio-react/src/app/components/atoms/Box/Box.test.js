57eeb716ea917505bb6b533c43b600b06fb7e936
Mia <maria.sgr@gmail.com>
2021-08-30 21:23:44 +0200

    Add tests to Box component

---
+++ portfolio-react/src/app/components/atoms/Box/Box.test.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render } from "@testing-library/react";
+
+import { Box } from "./Box";
+
+describe("Check box", () => {
+    let component;
+    beforeEach(() => {
+        component = render(
+        <Box>
+            <div>This is an example</div>
+        </Box>
+        );
+    })
+
+    test("renders its children", () => {
+        component.getByText("This is an example");
+    });
+});
