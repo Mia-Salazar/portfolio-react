eb1a7daa1a613d82583761a21757b77369d4185a
Mia <maria.sgr@gmail.com>
2021-09-02 20:49:26 +0200

    Add tests to Button

---
+++ portfolio-react/src/app/components/atoms/Button/Button.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "@testing-library/jest-dom/extend-expect";
+import { fireEvent, render } from "@testing-library/react";
+
+import i18n from "../../../../i18n";
+import { Button } from "./Button";
+
+
+describe('<Button />', () => {
+    const buttonText = "Send";
+    i18n.changeLanguage('cimode');
+  
+    test("is disabled", () => {
+        const component = render(
+            <Button text={buttonText} disabled={true}/>,
+        );
+        const button = component.getByText("Send");
+        expect(button).toBeDisabled();
+    });
+
+    test("click works", async() => {
+        const mockHandler = jest.fn();
+        const component = render(
+            <Button text={buttonText} functionality={mockHandler} />
+        );
+        const button = component.getByText("Send");
+        fireEvent.click(button);
+        expect(mockHandler).toHaveBeenCalledTimes(1);
+    });
+});
