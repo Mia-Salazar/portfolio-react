3e5329711a278f348507c2032ed938de36685a98
Mia-Salazar <maria.sgr@gmail.com>
2023-03-25 15:37:55 +0100

    Change name of articles

--- src/app/utils/data/awardsArray.js
+++ src/app/utils/data/awardsArray.js
@@ -5,11 +5,13 @@ export const awardsArray = [
 	{ lang: "es", aria: "awards.talks", icon: "microphone", target: "_blank", text: "awards.talksTwo", href: "https://www.youtube.com/watch?v=3S9at2lYPjA", type: "others" },
 	{ lang: "es", aria: "awards.talks", icon: "venus", target: "_blank", text: "awards.talksOne", href: "https://eventos.uc3m.es/70884/section/33597/ciencia-y-tecnologia-en-femenino.-programa-e-inauguracion-exposicion.html", type: "others" },
 	{ lang: "es", aria: "awards.interviewText", icon: "venus", target: "_blank", text: "awards.interview", href: "https://www.youtube.com/watch?v=ljT-Zwobxos&feature=emb_title&ab_channel=ITgals", type: "others" },
-	{ lang: "en", aria: "awards.programming", icon: "file-alt", target: "_blank", text: "awards.articleOne", href: "https://medium.com/@marasalazar/developers-nightmare-web-accessibility-52b9a6890403", type: "article" },
-	{ lang: "en", aria: "awards.programming", icon: "file-alt", target: "_blank", text: "awards.articleTwo", href: "https://medium.com/@marasalazar/why-icomoon-why-15c2d78ec96e", type: "article"  },
-	{ lang: "en", aria: "awards.programming", icon: "file-alt", target: "_blank", text: "awards.articleThree", href: "https://marasalazar.medium.com/how-to-structure-css-in-vue-like-a-pro-5475749cf5ea", type: "article"  },
-	{ lang: "es", aria: "awards.programming", icon: "file-alt", target: "_blank", text: "awards.articleFive", href: "https://marasalazar.medium.com/c%C3%B3mo-dar-soporte-a-ie-y-no-morir-en-el-intento-7049dcbbf43f", type: "article"  },
-	{ lang: "en", aria: "awards.programming", icon: "file-alt", target: "_blank", text: "awards.articleFour", href: "https://marasalazar.medium.com/to-angular-material-or-not-to-angular-material-940a616ac22d", type: "article"  },
+	
+	{ lang: "en", aria: "awards.programming", icon: "file-alt", target: "_blank", text: "A Developer’s Nightmare: Web Accessibility", href: "https://medium.com/@marasalazar/developers-nightmare-web-accessibility-52b9a6890403", type: "article" },
+	{ lang: "en", aria: "awards.programming", icon: "file-alt", target: "_blank", text: "Why, Icomoon, why?", href: "https://medium.com/@marasalazar/why-icomoon-why-15c2d78ec96e", type: "article"  },
+	{ lang: "en", aria: "awards.programming", icon: "file-alt", target: "_blank", text: "How to Structure CSS in Vue Like a Pro", href: "https://marasalazar.medium.com/how-to-structure-css-in-vue-like-a-pro-5475749cf5ea", type: "article"  },
+	{ lang: "es", aria: "awards.programming", icon: "file-alt", target: "_blank", text: "Cómo dar soporte a IE y no morir en el intento", href: "https://marasalazar.medium.com/c%C3%B3mo-dar-soporte-a-ie-y-no-morir-en-el-intento-7049dcbbf43f", type: "article"  },
+	{ lang: "en", aria: "awards.programming", icon: "file-alt", target: "_blank", text: "To Angular Material or not to Angular Material?", href: "https://marasalazar.medium.com/to-angular-material-or-not-to-angular-material-940a616ac22d", type: "article"  },
+	
 	{ lang: "en", aria: "awards.cybersecurity", icon: "shield-alt", target: "_blank", text: "The 9 most common types of malware and how to protect from them.", href: "https://blog.enthec.com/the-9-most-common-types-of-malware-and-how-to-protect-from-them/", type: "article" },
 	{ lang: "en", aria: "awards.cybersecurity", icon: "shield-alt", target: "_blank", text: "The importance of maintaining the health of your DNS", href: "https://blog.enthec.com/the-importance-of-maintaining-the-health-of-your-dns/", type: "article"  },
 	{ lang: "en", aria: "awards.cybersecurity", icon: "shield-alt", target: "_blank", text: "Discover how to protect your company from web exposition with Kartosn", href: "https://blog.enthec.com/discover-how-to-protect-your-company-from-web-exposition-with-kartos/", type: "article"  },
@@ -24,6 +26,7 @@ export const awardsArray = [
 	{ lang: "en", aria: "awards.cybersecurity", icon: "shield-alt", target: "_blank", text: "What is the difference between Spam and Phishing?", href: "https://blog.enthec.com/what-is-google-hacking/", type: "article"  },
 	{ lang: "en", aria: "awards.cybersecurity", icon: "shield-alt", target: "_blank", text: "What is Google Hacking?", href: "https://blog.enthec.com/cybersecurity-in-front-end-development/", type: "article"  },
 	{ lang: "en", aria: "awards.cybersecurity", icon: "shield-alt", target: "_blank", text: "Cybersecurity in front-end development", href: "https://blog.enthec.com/the-6-types-of-hat-hackers/", type: "article"  },
+	
 	{ lang: "es", aria: "awards.videogames", icon: "gamepad", target: "_blank", text: "awards.videogame", href: "https://xcjgames.itch.io/treeventure", type: "others" },
 	{ lang: "es", aria: "awards.videogames", icon: "gamepad", target: "_blank", text: "awards.videogameTwo", href: "https://xcjgames.itch.io/damng-la-balada-del-capitan", type: "others" },
 ];
