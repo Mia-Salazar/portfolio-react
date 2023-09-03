72d47e509c3b5eae3a88f91b50822733601c68a0
Mia-Salazar <maria.sgr@gmail.com>
2023-09-03 14:36:33 +0200

    Change skip to cotnent

--- src/app/components/organisms/NavbarContainer/NavbarContainer.js
+++ src/app/components/organisms/NavbarContainer/NavbarContainer.js
@@ -6,21 +6,11 @@ import { Navbar } from "../../organisms/Navbar/Navbar";
 
 import "./NavbarContainer.scss";
 
-export const NavbarContainer = ({openMenu, menuOpen, menuOpenFirstTime, activeLink, mainRef, changeRoute}) => {
-	const changeFocus = () => {
-		changeRoute();
-		mainRef.current.focus();
-	};
-
+export const NavbarContainer = ({openMenu, menuOpen, menuOpenFirstTime, activeLink}) => {
 	return (
 		<div className="navbar-container">
 			{ menuOpen && 
-				<>
-					<div className="navbar-container__skip-wrapper">
-						<Button functionality={changeFocus} text="skip" modificator="small" />
-					</div>
-					<Navbar functionality={openMenu} activeLink={activeLink}/>
-				</> 
+				<Navbar functionality={openMenu} activeLink={activeLink}/>
 			}
 			{ menuOpen && menuOpenFirstTime && <Button text="navbar.back" functionality={openMenu}></Button> }
 			{ !menuOpen && menuOpenFirstTime && <Button text="navbar.menu" functionality={openMenu} modificator="back"></Button> }
@@ -29,12 +19,10 @@ export const NavbarContainer = ({openMenu, menuOpen, menuOpenFirstTime, activeLi
 };
 
 NavbarContainer.propTypes = {
-	mainRef: PropTypes.object.isRequired,
 	menuOpen: PropTypes.bool.isRequired,
 	menuOpenFirstTime: PropTypes.bool.isRequired,
 	openMenu: PropTypes.func.isRequired,
 	activeLink: PropTypes.string,
-	changeRoute: PropTypes.func.isRequired,
 };
 
 NavbarContainer.displayName = "NavbarContainer";
